2be42c5f85ffb364668be2fcbaa734868b80f82f
DevHimanshuK <himanshudevelopment@gmail.com>
2022-12-13 16:56:02 +0530

    Added All the data related configs like context, migrations, connection string.

---
+++ NZWalks/NZWalks.API/Data/NZWalksDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Data
+{
+    public class NZWalksDbContext : DbContext
+    {
+        //DbContext class Constructor
+        //ctor 2*tab shortcut
+        public NZWalksDbContext(DbContextOptions<NZWalksDbContext> options) : base(options) //passing options to the base class
+        {
+
+        }
+
+        //3 DbSet type properties
+
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; } //This is a lookup table so singular
+    }
+}
